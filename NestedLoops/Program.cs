034beef659ebb6e6947dfacc752312ad00750a66
Nathan Strang <n.strang95@gmail.com>
2024-01-10 09:07:39 +0000

    Adding program code

---
+++ NestedLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Nested loops is essentially a loop within a loop
+             * They are used for a variety of things, such as sorting algorithims 
+             */ 
+
+            Console.Write("How many rows?: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many columns?: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What symbol or character would you like to use?: ");
+            String symbol = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++) 
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
